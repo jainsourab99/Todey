d0179d8b260a4af3f8fda88844f24c54d0fcc453
Sourabh Jain <jainsourab.99@gmail.com>
2019-07-15 13:35:36 +0530

    Add new Functionality to code

--- Todey/TodoListVC.swift
+++ Todey/TodoListVC.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListVC: UITableViewController {
     
-    let item = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+    var item = ["Find Mike","Buy Eggos","Destory Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +42,28 @@ class TodoListVC: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    //MARK:- Add new Items
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.item.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+    }
     
     
 
